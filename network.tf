32b5fb4ce30dd32228becd9b125cae34c760510b
slaurijssen <serve.laurijssen@fujifilm.com>
2021-12-01 14:05:35 +0100

    2 VM's with own public ip

--- network.tf
+++ network.tf
@@ -13,24 +13,53 @@ resource "azurerm_subnet" "subnet-internal-1" {
 }
 
 resource "azurerm_network_security_group" "allow-ssh" {
-   name                    = "${var.prefix}-allow-ssh"
-   location                = var.location
-   resource_group_name     = azurerm_resource_group.geofriends.name
+  name                = "${var.prefix}-allow-ssh"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.geofriends.name
 
-   security_rule {
-       name                    = "SSH"
-       priority                = 1001
-       direction               = "Inbound"
-       access                  = "Allow"
-       protocol                = "Tcp"
-       source_port_range       = var.ssh-source-address
-       destination_port_range       = var.ssh-destination-address
-       source_address_prefix        = var.ssh-source-address
-       destination_address_prefix   = var.ssh-destination-address
-   }
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = var.ssh-source-address
+    destination_port_range     = var.ssh-destination-address
+    source_address_prefix      = var.ssh-source-address
+    destination_address_prefix = var.ssh-destination-address
+  }
 }
 
-resource "azurerm_subnet_network_security_group_association" "sec-group-association" {
+resource "azurerm_network_security_group" "internal-facing" {
+  name                = "${var.prefix}-internal-facing"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.geofriends.name
+
+  security_rule {
+    name                                  = "test-rule"
+    priority                              = 1001
+    direction                             = "Inbound"
+    access                                = "Allow"
+    protocol                              = "Tcp"
+    source_port_range                     = "*"
+    destination_port_range                = "80"
+    source_application_security_group_ids = [azurerm_application_security_group.geo-appsec-group.id]
+    destination_address_prefix            = "*"
+  }
+  security_rule {
+    name                       = "test-rule-deny"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "sec-group-association-1" {
   subnet_id                 = azurerm_subnet.subnet-internal-1.id
   network_security_group_id = azurerm_network_security_group.allow-ssh.id
 }
